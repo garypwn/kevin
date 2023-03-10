f024967b247e24e67e95939e057ed12515488497
Gary Parsons <39571634+garypwn@users.noreply.github.com>
2023-03-10 02:48:15 -0800

    Define observation and action spaces for snakes

--- kevin/environment/snake_engine.py
+++ kevin/environment/snake_engine.py
@@ -1,7 +1,9 @@
 from abc import abstractmethod, ABC
 from typing import List, Tuple, Dict, Optional
 
+import gymnasium
 import jax.numpy as jnp
+from gymnasium import spaces
 from pettingzoo import ParallelEnv
 from pettingzoo.utils.env import ActionDict, ObsDict
 
@@ -29,12 +31,34 @@ class SnakeEngine(ABC):
 
 
 class MultiSnakeEnv(ParallelEnv):
-    r"""Takes a SnakeEngine and maps it to a pettingzoo environment"""
+    r"""Takes a SnakeEngine and maps it to a pettingzoo environment.
+    Observations are of the form
+        "snakes": [id: 0-15]["health": 0-100, "squad": 0-3, "you": T-F]
+        "board": [x: 0-w][y:0-h][cell: empty=0, food=1, hazard=2, snake head = 2(id)+3, body= 2(id)+4 => 0 .. 30]
+    """
 
     snake: SnakeEngine
 
     def __init__(self, eng: SnakeEngine):
         self.snake = eng
+
+        self.possible_agents = ["snake_" + str(r) for r in range(16)]
+        self.action_spaces = {agent: spaces.Discrete(4) for agent in self.possible_agents}
+
+    def observation_space(self, agent) -> gymnasium.spaces.Space:
+        return spaces.Dict(
+            # todo: expand to variable sized board, larger number of snakes
+            {
+                "snakes": spaces.Tuple(((spaces.Dict(
+                    {
+                        "health": spaces.Box(0, 100, dtype=int),
+                        "squad": spaces.Discrete(4),
+                        "you": spaces.Discrete(2)
+                    }
+                )),)*4),  # There are 4 snakes for now
+                "board": spaces.Box(low=jnp.ndarray([0, 0]), high=jnp.ndarray([10, 10]), dtype=int),
+            }
+        )
 
     def reset(self, seed: Optional[int] = None, return_info: bool = False, options: Optional[dict] = None) -> ObsDict:
         pass
