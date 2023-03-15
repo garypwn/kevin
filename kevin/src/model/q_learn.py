640e56520356150dba4afdb1d5cfc5d589a05901
Gary Parsons <garyparsons@uvic.ca>
2023-03-14 21:28:01 -0700

    Implement Q-learning. It doesn't really work...

--- kevin/src/model/q_learn.py
+++ kevin/src/model/q_learn.py
@@ -35,18 +35,39 @@ def func_q(S, is_training):
 q = coax.Q(func_q, gym_env)
 pi = coax.BoltzmannPolicy(q, temperature=0.1)
 
-obs = env.reset()
-print(env.render())
-while len(env.agents) > 0:
-    a_dict = {}
+# One tracer for each agent
+tracers = {agent: coax.reward_tracing.NStep(n=8, gamma=0.9) for agent in env.possible_agents}
 
-    # Submit actions
-    for agent in env.agents:
-        a_dict[agent] = pi(obs[agent])
+# Updater
+qlearning = coax.td_learning.QLearning(q)
 
-    obs_next, r_dict, terminations, truncations, infos = env.step(a_dict)
-    print(env.render())
+for i in range(1000000):
 
-    # todo: Update policy here
+    # Episode
+    obs = env.reset()
 
-    obs = obs_next
+    if i % 800 == 0:
+        print("===== Game {} =========================".format(i))
+        print(env.render())
+
+    while len(env.agents) > 0:
+        a_dict = {}
+
+        # Submit actions
+        for agent in env.agents:
+            a_dict[agent] = pi(obs[agent])
+
+        obs_next, r_dict, terminations, truncations, infos = env.step(a_dict)
+
+        # Trace rewards
+        for agent in env.agents:
+            tracers[agent].add(obs[agent], a_dict[agent], r_dict[agent], terminations[agent] or truncations[agent])
+
+        # Update q-learning
+        for _, tracer in tracers.items():
+            while tracer:
+                qlearning.update(tracer.pop())
+
+        if i % 800 == 0:
+            print(env.render())
+        obs = obs_next
